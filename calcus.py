af8c9f38d99c5222abfb56680c57643ad11138f2
EZ4ZZW <zhiwayzhang@outlook.com>
2021-04-20 13:21:27 +0800

    fix: use numpy.sin not math.sin

--- calcus.py
+++ calcus.py
@@ -1,14 +1,13 @@
 import numpy as np
-import math as m
 
 def f(x):
-    return m.sin(x)/x
+    return np.sin(x)
 
 def Calc():
     eps = 1e-7
     Flag = True
-    l = 0.0
-    r = 1.0
+    l = 1.0
+    r = 2.0
     h = r - l
     t1 = 1.0*(h/2)*(1+f(r))
     t2 = 0.0
